817fedee61862ce20d638da9c7d96818cb8c3fbe
Kyle Munz on Snoopy <kyle138@gmail.com>
2017-07-26 16:14:21 -0500

    refresh code moved to requestRefreshToken()

--- home/home.js
+++ home/home.js
@@ -41,6 +41,68 @@ angular.module("homeApp", [])
      }); */
   };  // End getTables
 
+  function requestRefreshToken(cb) {
+    if(!localStorage["google_refresh_token"]) {  // google_refresh_token missing
+      console.log("Unable to refresh without google_refresh_token, logging out");
+      $location.url("/login");
+    } else {
+      console.log("localStorage.google_refresh_token found...");  // DEBUG
+      if(!localStorage["google_access_token"]) {  // google_access_token missing
+        console.log("Unable to refresh without google_access_token, logging out");
+        $location.url("/login");
+      } else {
+        console.log("localStorage.google_access_token found..."); // DEBUG
+        console.log("Requesting refresh token...");
+        $http.get(  // Query API gateway for refreshToken
+          configuration.apigRefreshToken,
+          {
+            params:  {
+                refreshToken: localStorage.getItem('google_refresh_token'),
+                accessToken: localStorage.getItem('google_access_token')
+            }
+          }
+        ).then(function(response) {
+          if(response.data.errorMessage) {
+            console.log("Refresh request unsuccessful, logging out.");
+            console.log("response.data.errorMessage: "+response.data.errorMessage);
+            $location.url("/login");
+          } else {
+            if(!AWS.config.credentials) {
+              console.log("get apigRefreshToken response: "+JSON.stringify(response,null,2));  //DEBUG
+              console.log("Creating AWS.config.credentials");
+              var cognitoParams = {
+                IdentityPoolId: configuration.IdentityPoolId,
+                Logins: {
+                  'accounts.google.com': response.data.id_token
+                },
+                LoginId: awstoken.emailAdrs
+              };
+              AWS.config.region = 'us-east-1';
+              AWS.config.credentials = new AWS.CognitoIdentityCredentials(cognitoParams);
+            }
+            AWS.config.credentials.refresh(function(err) {
+              if(err) {
+                console.log("AWS.config.credentials.refresh Error: "+err);
+                $location.url("/login");
+              } else {
+                console.log("refresh.cognitoIdentityId "+AWS.config.credentials.identityId);
+                console.log("refresh.accessKeyId "+AWS.config.credentials.accessKeyId);
+                console.log("refresh.secretAccessKey "+AWS.config.credentials.secretAccessKey);
+                console.log("refresh.sessionToken "+AWS.config.credentials.sessionToken);
+                console.log("refresh.expireTime "+AWS.config.credentials.expireTime);
+                awstoken.cognitoIdentityId = {'S': AWS.config.credentials.identityId};
+                awstoken.accessKeyId = {'S': AWS.config.credentials.accessKeyId};
+                awstoken.secretAccessKey = {'S': AWS.config.credentials.secretAccessKey};
+                awstoken.sessionToken = {'S': AWS.config.credentials.sessionToken};
+                awstoken.expireTime = {'S': AWS.config.credentials.expireTime};
+              }
+            }); // End AWS.config.credentials.refresh()
+          } // If..Else errorMessage from apigRefreshToken
+        }); // End http.get refreshToken
+      } // end if(google_access_token)
+    } // end if(google_refresh_token)
+  }; // End requestRefreshToken
+
   // 'tis the logout button
   $scope.logout = function() {
     localStorage.removeItem('awstoken');
@@ -53,84 +115,20 @@ angular.module("homeApp", [])
     $location.url("/login");
   } else {  // Retreive awstoken and check if expired
     var awstoken = JSON.parse(localStorage.getItem('awstoken'));
-    if(new Date() > new Date(awstoken.expireTime)) {  // Token is expired, redirect to /login
-      console.log("Token Expired");
-      $http.get(  // Query API gateway for refreshToken
-        configuration.apigRefreshToken,
-        {
-          params:  {
-              refreshToken: localStorage.getItem('google_refresh_token'),
-              accessToken: localStorage.getItem('google_access_token')
-          }
-        }
-      ).then(function(response) {
-        if(!AWS.config.credentials) {
-          console.log("Creating AWS.config.credentials");
-          var cognitoParams = {
-            IdentityPoolId: configuration.IdentityPoolId,
-            Logins: {
-              'accounts.google.com': response.data.id_token
-            },
-            LoginId: awstoken.emailAdrs
-          };
-          AWS.config.region = 'us-east-1';
-          AWS.config.credentials = new AWS.CognitoIdentityCredentials(cognitoParams);
-        }
-        AWS.config.credentials.refresh(function(err) {
-          if(err) {
-            console.log("AWS.config.credentials.refresh Error: "+err);
-            return;
-          } else {
-            console.log("refresh.cognitoIdentityId "+AWS.config.credentials.identityId);
-            console.log("refresh.accessKeyId "+AWS.config.credentials.accessKeyId);
-            console.log("refresh.secretAccessKey "+AWS.config.credentials.secretAccessKey);
-            console.log("refresh.sessionToken "+AWS.config.credentials.sessionToken);
-            console.log("refresh.expireTime "+AWS.config.credentials.expireTime);
-          }
-        });
-      }); // End http.get refreshToken
-
-      //$location.url("/login");
-      //****************************//
+    var rightNow = new Date();
+    // If awstoken is after expireTime but before refreshExpire10, attempt to refresh
+    if(rightNow > new Date(awstoken.expireTime)) {
+      if(rightNow < new Date(awstoken.refreshExpire10)) {
+        console.log("Token Expired, attempting to refresh.");
+        requestRefreshToken();
+      } else {
+        console.log("Token Expired, refresh attempts expired.");
+        localStorage.removeItem('awstoken');
+        $location.url("/login");
+      }
     } else {  // Token is current, proceed.
       console.log("Token is still fresh");
-      var refresh=localStorage.getItem('google_refresh_token');
-      var access=localStorage.getItem('google_access_token');
-      $http.get(  // Query API gateway for refreshToken
-        configuration.apigRefreshToken,
-        {
-          params:  {
-              refreshToken: refresh,
-              accessToken: access
-          }
-        }
-      ).then(function(response) {
-        if(!AWS.config.credentials) {
-          console.log("Creating AWS.config.credentials");
-          var cognitoParams = {
-            IdentityPoolId: configuration.IdentityPoolId,
-            Logins: {
-              'accounts.google.com': response.data.id_token
-            },
-            LoginId: awstoken.emailAdrs
-          };
-          AWS.config.region = 'us-east-1';
-          AWS.config.credentials = new AWS.CognitoIdentityCredentials(cognitoParams);
-        }
-        AWS.config.credentials.refresh(function(err) {
-          if(err) {
-            console.log("AWS.config.credentials.refresh Error: "+err);
-            return;
-          } else {
-            console.log("refresh.cognitoIdentityId "+AWS.config.credentials.identityId);
-            console.log("refresh.accessKeyId "+AWS.config.credentials.accessKeyId);
-            console.log("refresh.secretAccessKey "+AWS.config.credentials.secretAccessKey);
-            console.log("refresh.sessionToken "+AWS.config.credentials.sessionToken);
-            console.log("refresh.expireTime "+AWS.config.credentials.expireTime);
-          }
-        });
-      }); // End http.get refreshToken
-
+      requestRefreshToken();
 /*      invokeLambda(null, awstoken, function(res) {
         $scope.$apply(function() {
           $scope.salutations = "Hello "+awstoken.emailAdrs;
